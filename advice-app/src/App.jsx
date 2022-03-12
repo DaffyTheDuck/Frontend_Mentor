4d7024ad0cd011b4618468ccf860273995ee7b3c
DaffyTheDuck <34976093+DaffyTheDuck@users.noreply.github.com>
2022-03-12 22:45:43 +0530

    Added Advice_Generator Challenge

---
+++ advice-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss'
+import RandomAdvice from './components/RandomAdvice';
+
+export default function App() {
+  return (
+    <div className='app-parent'>
+      <RandomAdvice />
+    </div>
+  )
+}
+
---
+++ advice-app/src/components/RandomAdvice.jsx
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react'
+import Card from './Card';
+import './RandomAdvice.scss'
+import axios from 'axios';
+import divider from '../assets/pattern-divider-desktop.svg';
+import dice from '../assets/icon-dice.svg';
+
+export default function RandomAdvice() {
+
+    const [advice, setAdvice] = useState('Hit To Get A Random Advice.');
+    const [adviceNumber, setAdviceNumber] = useState(0);
+
+    const ADVICE_API_URL = 'https://api.adviceslip.com/advice';
+
+    // fetching advice using axios
+
+    const fetchAdvice = async () => {
+        const response = await axios.get(ADVICE_API_URL);
+        setAdvice(response.data.slip.advice);
+        setAdviceNumber(response.data.slip.id);
+    }
+
+    return (
+        <>
+            <Card className='advice-card'>
+                <div className="advice-number">
+                    <p className='advice-number'>ADVICE #{adviceNumber}</p>
+                </div>
+                <div className="advice-div">
+                    <p className='advice'>
+                        “{advice}”
+                    </p>
+                </div>
+                <div className='divider-image-div'>
+                    <img src={divider} alt="desktop-divider" className='divider-image'/>
+                </div>
+                <div className="dice-container">
+                    <img src={dice} alt="dice" onClick={fetchAdvice}/>
+                </div>
+            </Card>
+        </>
+    );
+}
